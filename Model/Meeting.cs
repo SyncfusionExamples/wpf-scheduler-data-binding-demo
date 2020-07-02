433aa598ebe2f5bea5c4127b6e542e52fb13cf91
PiruthivirajM <44426156+PiruthivirajM@users.noreply.github.com>
2020-07-02 18:32:58 +0530

    sample files are added.

---
+++ Model/Meeting.cs
@@ -0,0 +1,97 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DataBinding
+{
+    public class Meeting : INotifyPropertyChanged
+    {
+        DateTime from, to;
+        string eventName;
+        bool isAllDay;
+        string startTimeZone, endTimeZone;
+        Brush color;
+        public Meeting()
+        {
+        }
+
+        public DateTime From
+        {
+            get { return from; }
+            set
+            {
+                from = value;
+                RaisePropertyChanged("From");
+            }
+        }
+
+        public DateTime To
+        {
+            get { return to; }
+            set
+            {
+                to = value;
+                RaisePropertyChanged("To");
+            }
+        }
+
+        public bool IsAllDay
+        {
+            get { return isAllDay; }
+            set
+            {
+                isAllDay = value;
+                RaisePropertyChanged("IsAllDay");
+            }
+        }
+        public string EventName
+        {
+            get { return eventName; }
+            set
+            {
+                eventName = value;
+                RaisePropertyChanged("EventName");
+            }
+        }
+        public string StartTimeZone
+        {
+            get { return startTimeZone; }
+            set
+            {
+                startTimeZone = value;
+                RaisePropertyChanged("StartTimeZone");
+            }
+        }
+        public string EndTimeZone
+        {
+            get { return endTimeZone; }
+            set
+            {
+                endTimeZone = value;
+                RaisePropertyChanged("EndTimeZone");
+            }
+        }
+
+        public Brush Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                RaisePropertyChanged("Color");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged(string propertyName, object oldValue = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ ViewModel/ScheduleViewModel.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using Syncfusion.UI.Xaml.Scheduler;
+using Syncfusion.Windows.Shared;
+using System.Linq;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Schedule View Model
+    /// </summary>
+    public class ScheduleViewModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// team management
+        /// </summary>
+        private List<string> teamManagement;
+
+        /// <summary>
+        /// current day meetings 
+        /// </summary>
+        private List<string> currentDayMeetings;
+
+        /// <summary>
+        /// minimum time meetings
+        /// </summary>
+        private List<string> minTimeMeetings;
+        /// <summary>
+        /// color collection
+        /// </summary>
+        private List<Brush> colorCollection;
+
+        /// <summary>
+        /// start time collection
+        /// </summary>
+        private List<DateTime> startTimeCollection;
+
+        /// <summary>
+        /// end time collection
+        /// </summary>
+        private List<DateTime> endTimeCollection;
+
+        /// <summary>
+        /// random numbers
+        /// </summary>
+        ////creating random number collection
+        private List<int> randomNums = new List<int>();
+        #endregion Properties
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScheduleViewModel" /> class.
+        /// </summary>
+        public ScheduleViewModel()
+        {
+            this.Events = new ObservableCollection<Meeting>();
+            this.CreateRandomNumbersCollection();
+            this.CreateStartTimeCollection();
+            this.CreateEndTimeCollection();
+            this.CreateSubjectCollection();
+            this.CreateColorCollection();
+            this.InitializeDataForBookings();
+            this.IntializeAppoitments();
+        }
+
+        #endregion Constructor
+
+        /// <summary>
+        /// Gets or sets appointments
+        /// </summary>
+        public ObservableCollection<Meeting> Events
+        {
+            get;
+            set;
+        }
+
+        #region Methods
+
+        #region GettingTimeRanges
+
+        /// <summary>
+        /// Method for get timing range.
+        /// </summary>
+        /// <returns>return time collection</returns>
+        private List<Point> GettingTimeRanges()
+        {
+            List<Point> randomTimeCollection = new List<Point>();
+            randomTimeCollection.Add(new Point(9, 11));
+            randomTimeCollection.Add(new Point(12, 14));
+            randomTimeCollection.Add(new Point(15, 17));
+
+            return randomTimeCollection;
+        }
+
+        #endregion GettingTimeRanges
+
+        #region InitializeDataForBookings
+
+        /// <summary>
+        /// Method for initialize data bookings.
+        /// </summary>
+        private void InitializeDataForBookings()
+        {
+            this.currentDayMeetings = new List<string>();
+            this.currentDayMeetings.Add("General Meeting");
+            this.currentDayMeetings.Add("Plan Execution");
+
+            // MinimumHeight Appointment Subjects
+            this.minTimeMeetings = new List<string>();
+            this.minTimeMeetings.Add("Work log alert");
+            this.minTimeMeetings.Add("Birthday wish alert");
+
+            this.colorCollection = new List<Brush>();
+            this.colorCollection.Add(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF339933")));
+            this.colorCollection.Add(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00ABA9")));
+            this.colorCollection.Add(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE671B8")));
+        }
+
+        #endregion InitializeDataForBookings
+
+        #region InitializeAppointments
+        /// <summary>
+        /// Initialize appointments
+        /// </summary>
+        /// <param name="count">count value</param>
+        private void IntializeAppoitments()
+        {
+            Random randomTime = new Random();
+            List<Point> randomTimeCollection = this.GettingTimeRanges();
+
+            DateTime date;
+            DateTime dateFrom = DateTime.Now.AddDays(-100);
+            DateTime dateTo = DateTime.Now.AddDays(100);
+            var random = new Random();
+            var dateCount = random.Next(4);
+            DateTime dateRangeStart = DateTime.Now.AddDays(0);
+            DateTime dateRangeEnd = DateTime.Now.AddDays(1);
+
+            for (date = dateFrom; date < dateTo; date = date.AddDays(1))
+            {
+                if (date.Day % 7 != 0)
+                {
+                    for (int additionalAppointmentIndex = 0; additionalAppointmentIndex < 1; additionalAppointmentIndex++)
+                    {
+                        Meeting meeting = new Meeting();
+                        int hour = randomTime.Next((int)randomTimeCollection[additionalAppointmentIndex].X, (int)randomTimeCollection[additionalAppointmentIndex].Y);
+                        meeting.From = new DateTime(date.Year, date.Month, date.Day, hour, 0, 0);
+                        meeting.To = meeting.From.AddHours(1);
+                        meeting.EventName = this.currentDayMeetings[randomTime.Next(1)];
+                        meeting.Color = this.colorCollection[randomTime.Next(2)];
+                        meeting.IsAllDay = false;
+                        meeting.StartTimeZone = string.Empty;
+                        meeting.EndTimeZone = string.Empty;
+                        this.Events.Add(meeting);
+                    }
+                }
+                else
+                {
+                    Meeting meeting = new Meeting();
+                    meeting.From = new DateTime(date.Year, date.Month, date.Day, randomTime.Next(9, 11), 0, 0);
+                    meeting.To = meeting.From.AddDays(2).AddHours(1);
+                    meeting.EventName = this.currentDayMeetings[randomTime.Next(1)];
+                    meeting.Color = this.colorCollection[randomTime.Next(2)];
+                    meeting.IsAllDay = true;
+                    meeting.StartTimeZone = string.Empty;
+                    meeting.EndTimeZone = string.Empty;
+                    this.Events.Add(meeting);
+                }
+            }
+
+            // Minimum Height Meetings
+            DateTime minDate;
+            DateTime minDateFrom = DateTime.Now.AddDays(-2);
+            DateTime minDateTo = DateTime.Now.AddDays(2);
+
+            for (minDate = minDateFrom; minDate < minDateTo; minDate = minDate.AddDays(1))
+            {
+                Meeting meeting = new Meeting();
+                meeting.From = new DateTime(minDate.Year, minDate.Month, minDate.Day, randomTime.Next(9, 18), 30, 0);
+                meeting.To = meeting.From;
+                meeting.EventName = this.minTimeMeetings[randomTime.Next(0, 1)];
+                meeting.Color = this.colorCollection[randomTime.Next(0, 2)];
+                meeting.StartTimeZone = string.Empty;
+                meeting.EndTimeZone = string.Empty;
+                
+                this.Events.Add(meeting);
+            }
+        }
+
+        #endregion InitializeAppointments
+
+        #region SubjectCollection
+
+        /// <summary>
+        /// Subject collection
+        /// </summary>
+        ////creating subject collection
+        private void CreateSubjectCollection()
+        {
+            this.teamManagement = new List<string>();
+            this.teamManagement.Add("General Meeting");
+            this.teamManagement.Add("Plan Execution");
+            this.teamManagement.Add("Project Plan");
+            this.teamManagement.Add("Consulting");
+        }
+
+        #endregion SubjectCollection
+
+        #region creating color collection
+
+        /// <summary>
+        /// color collection
+        /// </summary>
+        ////creating color collection
+        private void CreateColorCollection()
+        {
+            this.colorCollection = new List<Brush>();
+            this.colorCollection.Add(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFA2C139")));
+            this.colorCollection.Add(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD80073")));
+            this.colorCollection.Add(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF1BA1E2")));
+        }
+
+        #endregion creating color collection
+
+        #region CreateRandomNumbersCollection
+
+        /// <summary>
+        /// random numbers collection
+        /// </summary>
+        private void CreateRandomNumbersCollection()
+        {
+            this.randomNums = new List<int>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int random = rand.Next(9, 15);
+                this.randomNums.Add(random);
+            }
+        }
+
+        #endregion CreateRandomNumbersCollection
+
+        #region CreateStartTimeCollection
+
+        /// <summary>
+        /// start time collection
+        /// </summary>
+        //// creating StartTime collection
+        private void CreateStartTimeCollection()
+        {
+            this.startTimeCollection = new List<DateTime>();
+            DateTime currentDate = DateTime.Now;
+
+            int count = 0;
+            for (int i = -5; i < 5; i++)
+            {
+                DateTime startTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, this.randomNums[count], 0, 0);
+                DateTime startDateTime = startTime.AddDays(i);
+                this.startTimeCollection.Add(startDateTime);
+                count++;
+            }
+        }
+
+        #endregion CreateStartTimeCollection
+
+        #region CreateEndTimeCollection
+
+        /// <summary>
+        /// end time collection
+        /// </summary>
+        ////  creating EndTime collection
+        private void CreateEndTimeCollection()
+        {
+            this.endTimeCollection = new List<DateTime>();
+            DateTime currentDate = DateTime.Now;
+            int count = 0;
+            for (int i = -5; i < 5; i++)
+            {
+                DateTime endTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, this.randomNums[count] + 1, 0, 0);
+                DateTime endDateTime = endTime.AddDays(i);
+                if (i == -3 || i == 3)
+                {
+                    endDateTime = endTime.AddDays(i).AddHours(22);
+                }
+
+                this.endTimeCollection.Add(endDateTime);
+                count++;
+            }
+        }
+
+        #endregion CreateEndTimeCollection
+
+        #endregion Methods
+    }
+}
